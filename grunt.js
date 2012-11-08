22d42822d667481ccaa5b8bb3bd31f403cf987e8
Andrey Savchenko <andrey@aejis.eu>
2012-11-08 20:28:01 +0200

    Add browser to min task

--- grunt.js
+++ grunt.js
@@ -36,11 +36,16 @@ module.exports = function( grunt ) {
 
     // Lists of files to be minified with UglifyJS, used by the "min" task.
     min: {
+      // Base
       util:       { src: ["src/base/util.js"],      dest: "dist/minified/base/util.js" },
       attribute:  { src: ["src/base/attribute.js"], dest: "dist/minified/base/attribute.js" },
       event:      { src: ["src/base/event.js"],     dest: "dist/minified/base/event.js" },
       enumerable: { src: ["src/base/enumerable.js"],dest: "dist/minified/base/enumerable.js" },
-      promise:    { src: ["src/base/promise.js"],   dest: "dist/minified/base/promise.js" }
+      promise:    { src: ["src/base/promise.js"],   dest: "dist/minified/base/promise.js" },
+
+      // Browser
+      dom: { src: ["src/browser/dom.js"], dest: "dist/minified/browser/dom.js" },
+      cookie: { src: ["src/browser/cookieStore.js"], dest: "dist/minified/browser/cookieStore.js" }
     },
 
     // Global configuration options for UglifyJS.
