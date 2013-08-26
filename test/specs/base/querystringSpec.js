519b7b36c9402dac330f197bb727007341616db3
Andrey Savchenko <andrey@aejis.eu>
2013-08-26 21:54:59 +0300

    Change types for decoded qs

--- test/specs/base/querystringSpec.js
+++ test/specs/base/querystringSpec.js
@@ -211,7 +211,7 @@ define(['base/querystring'], function(QueryString) {
         var result = QueryString.parse('a=1&b[0]=1&b[1][0]=5&b[1][1][f]=false&b[1][1][s]=7&b[2]=3&c=false&d[a]=1&d[b]=2');
 
         expect(result.a).to.be.eql(1);
-        expect(result.b).to.be.eql(['1', [5, {f: false, s: '7'}], 3]);
+        expect(result.b).to.be.eql([1, [5, {f: false, s: 7}], 3]);
       });
 
       it('should parse object with custom delimiter', function() {
@@ -219,7 +219,7 @@ define(['base/querystring'], function(QueryString) {
             result = QueryString.parse(str, { delimiter: ';' });
 
         expect(result.a).to.be.eql(1);
-        expect(result.b).to.be.eql(['1', [5, {f: false, s: '7'}], 3]);
+        expect(result.b).to.be.eql([1, [5, {f: false, s: 7}], 3]);
       });
 
       it ('should parse object with custom key-value delimiter', function() {
@@ -227,7 +227,7 @@ define(['base/querystring'], function(QueryString) {
             result = QueryString.parse(str, { eq: ':' });
 
         expect(result.a).to.be.eql(1);
-        expect(result.b).to.be.eql(['1', [5, {f: false, s: '7'}], 3]);
+        expect(result.b).to.be.eql([1, [5, {f: false, s: 7}], 3]);
       });
     });
 
